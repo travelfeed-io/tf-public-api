d2aed3061be3924c2e11649e1444ba476ded0eeb
tiotdev <tiotdev@tiot.de>
2020-06-08 17:03:46 +0200

    Set up serverless + apollo-server-lambda

---
+++ graphql.js
@@ -0,0 +1,25 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: {
+    endpoint: '/dev/graphql',
+  },
+});
+
+exports.graphqlHandler = server.createHandler();
